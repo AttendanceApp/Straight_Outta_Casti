ba8dbd4bdfac85a4ff18c55d374a1c229b466629
Cantevisa <18vwoo@castilleja.org>
2017-03-06 14:25:39 -0800

    Actually fixed the error. :)

--- Straight Outta Casti/SignOutViewController.swift
+++ Straight Outta Casti/SignOutViewController.swift
@@ -1,25 +1,37 @@
 //
-//  ViewController.swift
+//  SignOutViewController.swift
 //  Straight Outta Casti
 //
-//  Created by Vanessa Woo on 2/16/17.
+//  Created by Vanessa Woo on 3/6/17.
 //  Copyright © 2017 Attendance App. All rights reserved.
 //
 
 import UIKit
 
 class SignOutViewController: UIViewController {
-
-//    let geofence: Geofence = Geofence(latitudeDeadband: Constants.Geolocation.latitudeDeadband, longitudeDeadband: Constants.Geolocation.longitudeDeadband, targetLatitude: Constants.Geolocation.castiLatitude, targetLongitude: Constants.Geolocation.castiLongitude)
+    
     var stateController: StateController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
