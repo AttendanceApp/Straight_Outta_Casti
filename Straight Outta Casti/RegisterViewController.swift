4f0748e8c6c06b86a0c755036b2840338bc160f0
Cantevisa <18vwoo@castilleja.org>
2017-03-09 13:46:55 -0800

    FIXED THE ISSUE!!!!!!!!!!!!!!!! Now the state controller is passed along by the RegisterViewController to the SignOutViewController and all is well.

--- Straight Outta Casti/RegisterViewController.swift
+++ Straight Outta Casti/RegisterViewController.swift
@@ -34,8 +34,11 @@ class RegisterViewController: UIViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "submitRegistration" {
+            let controller = segue.destination as! SignOutViewController
+            controller.stateController = self.stateController
+        }
+    }
 
 }
