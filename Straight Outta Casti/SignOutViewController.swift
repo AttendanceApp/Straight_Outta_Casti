3ca4768c1177a4ce8b0951579ba8a81e458f570a
Cantevisa <18vwoo@castilleja.org>
2017-03-13 10:20:33 -0700

    fixed!!!!

--- Straight Outta Casti/SignOutViewController.swift
+++ Straight Outta Casti/SignOutViewController.swift
@@ -15,16 +15,16 @@ class SignOutViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Thumba.setupController()
+//        Thumba.setupController()
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        Thumba.updateUI()
+//        Thumba.updateUI()
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -36,15 +36,15 @@ class SignOutViewController: UIViewController {
             GoogleFormsConnection.doMyBidNiss(firstName: account.firstName, lastName: account.lastName, reason: reason.text!)
         }
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
