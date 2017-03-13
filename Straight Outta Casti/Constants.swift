e8834443b855d789a0981be31f28416ad1f90318
17eparker <lizparker@lizs-macbook-pro.local>
2017-03-13 10:49:16 -0700

    changed constants

--- Straight Outta Casti/Constants.swift
+++ Straight Outta Casti/Constants.swift
@@ -19,9 +19,9 @@ class Constants {
     struct GoogleForms {
         //add your strings for Google Forms here so that they're easy to access and change centrally
         //For example:
-        static let firstNameEntry: String = "entry.1266582852="
-        static let lastNameEntry: String = "&entry.2132189692="
-        static let reasonEntry: String = "&entry.1341441446="
-        static let url: String = "https://docs.google.com/a/castilleja.org/forms/d/e/1FAIpQLSdY6ks151DSYNytLyQ3aafNqvhmtF99MJPU95lAWVVRwt846g/formResponse"
+        static let firstNameEntry: String = "entry.42768537="
+        static let lastNameEntry: String = "&entry.1877164239="
+        static let reasonEntry: String = "&entry.674640187="
+        static let url: String = "https://docs.google.com/a/castilleja.org/forms/d/e/1FAIpQLSefSGzX46w4W8K_l3L68GndYLitfGvjA_u4jmVUWFbpIYmskA/formResponse"
     }
 }
