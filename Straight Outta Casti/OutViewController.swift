7f0378af95dea55a2893b4ca7f95759a0b1ab293
17eparker <lizparker@lizs-macbook-pro.local>
2017-03-29 11:34:19 -0700

    new

--- Straight Outta Casti/OutViewController.swift
+++ Straight Outta Casti/OutViewController.swift
@@ -39,6 +39,17 @@ class OutViewController: UIViewController {
         }
     }
     
+    // create the alert
+    let alert = UIAlertController(title: "UIAlertController", message: "Would you like to continue learning how to use iOS alerts?", preferredStyle: UIAlertControllerStyle.alert)
+    
+    // add the actions (buttons)
+    alert.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.default, handler: nil))
+    alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
+    
+    // show the alert
+    self.present(alert, animated: true, completion: nil)
+
+    
     /*
      // MARK: - Navigation
      
